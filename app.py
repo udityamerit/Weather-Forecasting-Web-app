e2b3c05d2e0c1a9f823968e11c70962a06dcad67
Uditya Narayan Tiwari <uditmerit@gmail.com>
2024-04-29 22:10:10 +0530

    updating the weather data

--- app.py
+++ app.py
@@ -95,94 +95,142 @@ if city == 'mumbai':
     st.image('mumbai.PNG',width= 1400)
     df2 = pd.DataFrame({'lat':[19.082],
                         'lon':[72.8789]})
-
     st.map(df2, size=200, color='#0044ff')
+    data = pd.DataFrame(
+        np.random.randn(10,3),
+        columns =['Temperature','Humidity','Pressure',])
+    st.header("""Graph of temp vs humidity vs pressure """)
+    st.line_chart(data)
 
 elif city == 'bhopal':
     st.header(" 2 Week Extended forecasting of weather 👇", divider='rainbow')
     st.image('bhopal.png',width= 1400)
     df2 = pd.DataFrame({'lat':[23.264],
                         'lon':[77.402]})
-
     st.map(df2, size=200, color='#FF5733')
+    data = pd.DataFrame(
+        np.random.randn(15,3),
+        columns =['Temperature','Humidity','Pressure',])
+    st.header("""Graph of temp vs humidity vs pressure """)
+    st.line_chart(data)
 
 elif city == 'tokyo':
     st.header(" 2 Week Extended forecasting of weather 👇", divider='rainbow')
     st.image('tokyo japan.PNG',width= 1400)
     df2 = pd.DataFrame({'lat':[35.6764],
                         'lon':[139.650]})
-
     st.map(df2, size=200, color='#FF5733')
+    data = pd.DataFrame(
+        np.random.randn(14,3),
+        columns =['Temperature','Humidity','Pressure',])
+    st.header("""Graph of temp vs humidity vs pressure """)
+    st.line_chart(data)
 
 elif city == 'london':
     st.header(" 2 Week Extended forecasting of weather 👇", divider='rainbow')
     st.image('london, uk, england.PNG',width= 1400)
     df2 = pd.DataFrame({'lat':[51.51279],
                         'lon':[-0.09184]})
-
     st.map(df2, size=200, color='#FF5733')
+    data = pd.DataFrame(
+        np.random.randn(19,3),
+        columns =['Temperature','Humidity','Pressure',])
+    st.header("""Graph of temp vs humidity vs pressure """)
+    st.line_chart(data)
 
 elif city == 'fatehpur':
     st.header(" 2 Week Extended forecasting of weather 👇", divider='rainbow')
     st.image('fatehpur.png',width= 1400)
-    df2 = pd.DataFrame({'lat':[51.51279],
-                        'lon':[-0.09184]})
-
+    df2 = pd.DataFrame({'lat':[25.9210],
+                        'lon':[80.7996]})
     st.map(df2, size=200, color='#FF5733')
-    
+    data = pd.DataFrame(
+        np.random.randn(35,3),
+        columns =['Temperature','Humidity','Pressure',])
+    st.header("""Graph of temp vs humidity vs pressure """)
+    st.line_chart(data) 
+
 elif city == 'kanpur':
     st.header(" 2 Week Extended forecasting of weather 👇", divider='rainbow')
     st.image('kanpurr.png',width= 1400)
-    df2 = pd.DataFrame({'lat':[51.51279],
-                        'lon':[-0.09184]})
+    df2 = pd.DataFrame({'lat':[26.464],
+                        'lon':[80.32]})
+    st.map(df2, size=200, color='#56F50C')
+    data = pd.DataFrame(
+        np.random.randn(25,3),
+        columns =['Temperature','Humidity','Pressure',])
+    st.header("""Graph of temp vs humidity vs pressure """)
+    st.line_chart(data)
 
-    st.map(df2, size=200, color='#FF5733')
 elif city == 'allahabad':
     st.header(" 2 Week Extended forecasting of weather 👇", divider='rainbow')
     st.image('prayag.png',width= 1400)
-    df2 = pd.DataFrame({'lat':[51.51279],
-                        'lon':[-0.09184]})
+    df2 = pd.DataFrame({'lat':[25.441],
+                        'lon':[81.835]})
+    st.map(df2, size=200, color='#F9FF33')
+    data = pd.DataFrame(
+        np.random.randn(14,3),
+        columns =['Temperature','Humidity','Pressure',])
+    st.header("""Graph of temp vs humidity vs pressure """)
+    st.line_chart(data)
 
-    st.map(df2, size=200, color='#FF5733')
 elif city == 'sehore':
     st.header(" 2 Week Extended forecasting of weather 👇", divider='rainbow')
     st.image('sehore.png',width= 1400)
-    df2 = pd.DataFrame({'lat':[51.51279],
-                        'lon':[-0.09184]})
+    df2 = pd.DataFrame({'lat':[23.2082],
+                        'lon':[77.0844]})
+    st.map(df2, size=200, color='#0CF5E0')
+    data = pd.DataFrame(
+        np.random.randn(12,3),
+        columns =['Temperature','Humidity','Pressure',])
+    st.header("""Graph of temp vs humidity vs pressure """)
+    st.line_chart(data)
 
-    st.map(df2, size=200, color='#FF5733')
 elif city == 'usa':
     st.header(" 2 Week Extended forecasting of weather 👇", divider='rainbow')
     st.image('usa.png',width= 1400)
-    df2 = pd.DataFrame({'lat':[51.51279],
-                        'lon':[-0.09184]})
+    df2 = pd.DataFrame({'lat':[40.3588],
+                        'lon':[-103.1743]})
 
-    st.map(df2, size=200, color='#FF5733')
+    st.map(df2, size=200, color='#E70CF5')
+    data = pd.DataFrame(
+        np.random.randn(14,3),
+        columns =['Temperature','Humidity','Pressure',])
+    st.header("""Graph of temp vs humidity vs pressure """)
+    st.line_chart(data)
 
 elif city == 'jaunpur':
     st.header(" 2 Week Extended forecasting of weather 👇", divider='rainbow')
     st.image('jaunpur.png',width= 1400)
     df2 = pd.DataFrame({'lat':[25.744],
                         'lon':[82.6837]})
-
     st.map(df2, size=200, color='#0044ff')
+    data = pd.DataFrame(
+        np.random.randn(10,3),
+        columns =['Temperature','Humidity','Pressure',])
+    st.header("""Graph of temp vs humidity vs pressure """)
+    st.line_chart(data)
 
 elif city == 'england':
     st.header(" 2 Week Extended forecasting of weather 👇", divider='rainbow')
     st.image('england.png',width= 1400)
     df2 = pd.DataFrame({'lat':[52.3555],
                         'lon':[1.1743]})
-
-    st.map(df2, size=200, color='#0044ff')
+    st.map(df2, size=200, color='#F50C56')
+    data = pd.DataFrame(
+        np.random.randn(15,3),
+        columns =['Temperature','Humidity','Pressure',])
+    st.header("""Graph of temp vs humidity vs pressure """)
+    st.line_chart(data)
 
 elif city == 'moscow':
     st.header(" 2 Week Extended forecasting of weather 👇", divider='rainbow')
     st.image('russia.png', width=1400)
-    st.write(df.head(9))
+    st.write(df.head(19))
     st.area_chart(df[['tempmax','tempmin']].head(9))
     df2 = pd.DataFrame({'lat':[55.755],
                         'lon':[37.617]})
-
-    st.map(df2, size=200, color='#0044ff')
+    st.map(df2, size=200, color='#F50C0C')
+    
 
    
